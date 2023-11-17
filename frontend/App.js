882308dd21fd2fd1bee5312cc93962b837c0fa63
jeremycollinsmpi <jeremycollinsgithub2@gmail.com>
2023-11-17 18:19:42 +0700

    use neo4j

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import CollapsibleButton from './CollapsibleButton';
+
+function App() {
+  return (
+    <div className="App">
+      <CollapsibleButton nodeId="1" buttonText="hello"/>
+    </div>
+  );
+}
+
+export default App;
